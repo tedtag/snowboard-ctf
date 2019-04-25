036737d63d99b8d129104d24dbfea164de91cfb4
Tagami <ted.t.tagami@accenture.com>
2019-04-25 16:53:11 -0700

    Adding stored XSS

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
     <footer class="page-footer font-small">
       <div class="footer-copyright text-center py-3">Copyright 2019 D.Lee</span>
     </footer>
-    <footer class="page-footer font-small" style="visibility: hidden">
+    <footer class="page-footer font-small" style="visibility: hidden; position: relative;">
       <div class="footer-copyright text-center py-3">Copyright 2019 D.Lee</span>
     </footer>
